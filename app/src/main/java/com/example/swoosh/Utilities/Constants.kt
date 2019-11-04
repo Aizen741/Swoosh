f3707251f696204f9d0b9e0b51f957eb19729fce
Aizen741 <rahulsivadas2009@gmail.com>
2019-11-04 08:59:07 +0530

    Pacelables

--- app/src/main/java/com/example/swoosh/Utilities/Constants.kt
+++ app/src/main/java/com/example/swoosh/Utilities/Constants.kt
@@ -2,5 +2,5 @@ package com.example.swoosh.Utilities
 
 import android.view.inputmethod.ExtractedText
 
-const val EXTRA_LEAGUE = "league"
-const val EXTRA_SKILL = "skill"
+
+const val EXTRA_PLAYER ="player"
